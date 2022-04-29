cdeb5f76b8487c6bb9eb1a7eebd5a632349e23a4
Lucas Tyman <99354994+Lucas-Tyman@users.noreply.github.com>
2022-04-29 01:50:52 +0000

    changes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ICS2O-PWA-Template
+# ICS2O-Assignment-04-
 
 [![Mr Coxall's Super Linter](https://github.com/Lucas-Tyman/ICS2O-Assignment-04-/workflows/Mr%20Coxall's%20Super%20Linter/badge.svg)](https://github.com/Lucas-Tyman/ICS2O-Assignment-04-/actions)
 
--- js/script.js
+++ js/script.js
@@ -11,9 +11,44 @@ if (navigator.serviceWorker) {
   })
 }
 
+;("use strict")
+
 /**
- * This function displays an alert.
+ * This function calculates the pizza price.
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+function total() {
+  // input
+  const large = document.getElementById("size-large").checked
+  const extraLarge = document.getElementById("size-extra-large").checked
+  const oneTopping = document.getElementById("first-topping").checked
+  const twoTopping = document.getElementById("two-toppings").checked
+  const threeTopping = document.getElementById("three-toppings").checked
+  const fourTopping = document.getElementById("four-toppings").checked
+
+  // process
+  const taxRate = 1.13
+  var pizzaSize = 0
+  var pizzaTopping = 0
+
+  if (large == true) {
+    pizzaSize = 6.0
+  } else if (extraLarge == true) {
+    pizzaSize = 10.0
+  }
+
+  if (oneTopping == true) {
+    pizzaTopping = 1.0
+  } else if (twoTopping == true) {
+    pizzaTopping = 1.75
+  } else if (threeTopping == true) {
+    pizzaTopping = 2.5
+  } else if (fourTopping == true) {
+    pizzaTopping = 3.35
+  }
+
+  totalCost = (pizzaSize + pizzaTopping) * taxRate
+
+  // output
+  document.getElementById("total-cost").innerHTML =
+    "The total cost of the pizza will be: $" + totalCost.toFixed(2)
 }
